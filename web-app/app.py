a02fc6d45efe9645c22c16ee469350dfc81d07ed
sarahswang <sarah.wang@gmail.com>
2025-04-23 13:57:45 -0400

    Setting up containers

---
+++ web-app/app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from pymongo import MongoClient
+import os
+
+app = Flask(__name__)
+
+# MongoDB setup
+MONGO_URI = os.getenv("MONGO_URI", "mongodb://mongodb:27017")
+client = MongoClient(MONGO_URI)
+db = client["gameDB"]
+users = db["users"]
+
+@app.route("/")
+def home():
+    return "Card Game Simulator is running! MongoDB connected."
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
