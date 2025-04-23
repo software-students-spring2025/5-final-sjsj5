82bd69c9a43fb8d264f62ea34c1b369330625b7f
schlichtings <sms10010@nyu.edu>
2025-04-23 14:27:10 -0400

    register function

--- web-app/app.py
+++ web-app/app.py
@@ -9,10 +9,12 @@ client = MongoClient(MONGO_URI)
 db = client["gameDB"]
 users = db["users"]
 
+# ROUTE
 @app.route("/")
 def home():
     return "Card Game Simulator is running! MongoDB connected."
 
+# FUNCTIONS
 def get_users():
     client = MongoClient("mongodb://db:27017/")
     print("Connected to MongoDB successfully.")
@@ -23,6 +25,19 @@ def get_users():
     for u in dbUsers:
         userDict[u["username"]] = {"password": u["password"]}
     return userDict
+
+def register_user(username, password):
+    client = MongoClient("mongodb://db:27017/")
+    print("Connected to MongoDB successfully.")
+    
+    collect = client["gameDB"]["users"]
+    collect.insert_one({
+        "username": username,
+        "password": password
+    })
+
+
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
